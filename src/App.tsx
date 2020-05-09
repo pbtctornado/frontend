3f05253b815d4ee006f60720a4f8db6c4b7206ca
robertbrada <bert.blader@gmail.com>
2020-05-09 12:02:54 +0200

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TypeScript App</h1>
+    </div>
+  );
+}
+
+export default App;
