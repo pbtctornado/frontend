808f2d0fd3c4f288ca98a05d5f0b3f490405c152
benesjan <janbenes1234@gmail.com>
2020-05-15 12:20:19 +0200

    snarks-functions.ts simplified

--- src/utils/snarks-functions.ts
+++ src/utils/snarks-functions.ts
@@ -1,18 +1,9 @@
-import crypto from 'crypto';
+const randomBytes = require('crypto').randomBytes;
 const circomlib = require('circomlib');
 const { bigInt } = require('snarkjs');
 
-// Deposit interface (use 'any' because I want to assign properties dynamically)
-interface Deposit {
-    commitment?: bigint;
-    nullifier?: any;
-    secret?: any;
-    preimage?: any;
-    nullifierHash?: bigint;
-}
-
 // Generate random number of specified byte length
-const rbigint = (nbytes: number) => bigInt.leBuff2int(crypto.randomBytes(nbytes));
+const rbigint = (nbytes: number) => bigInt.leBuff2int(randomBytes(nbytes));
 
 // Compute pedersen hash
 const pedersenHash = (data: object) => circomlib.babyJub.unpackPoint(circomlib.pedersenHash.hash(data))[0];
@@ -21,22 +12,20 @@ const pedersenHash = (data: object) => circomlib.babyJub.unpackPoint(circomlib.p
 const toHex = (number: any, length = 32) =>
     '0x' + (number instanceof Buffer ? number.toString('hex') : bigInt(number).toString(16)).padStart(length * 2, '0');
 
-// Create deposit object from secret and nullifier
-const createDeposit = (nullifier: bigint, secret: bigint) => {
-    let deposit: Deposit = { nullifier, secret };
-    deposit.preimage = Buffer.concat([deposit.nullifier.leInt2Buff(31), deposit.secret.leInt2Buff(31)]);
-    deposit.commitment = pedersenHash(deposit.preimage);
-    deposit.nullifierHash = pedersenHash(deposit.nullifier.leInt2Buff(31));
-    return deposit;
+const getNoteAndCommitment = () => {
+    const nullifier = rbigint(31);
+    const secret = rbigint(31);
+    // get snarks note and commitment
+    const preimage = Buffer.concat([nullifier.leInt2Buff(31), secret.leInt2Buff(31)]);
+    let commitment = pedersenHash(preimage);
+    const note: string = toHex(preimage, 62);
+    commitment = toHex(commitment);
+    return { note, commitment };
 };
 
 const getNoteStringAndCommitment = (btcAmount: number, chainId: number) => {
-    // get snarks note and commitment
-    const deposit = createDeposit(rbigint(31), rbigint(31));
-    const amount: number = btcAmount * 10 ** 3;
-    const note: string = toHex(deposit.preimage, 62);
-    const noteString: string = `tornado-eth-${amount}-${chainId}-${note}`;
-    const commitment: string = toHex(deposit.commitment);
+    const { note, commitment } = getNoteAndCommitment();
+    const noteString: string = `tornado-eth-${btcAmount * 10 ** 3}-${chainId}-${note}`;
     return { noteString, commitment };
 };
 
