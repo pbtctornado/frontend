554a2321c98ac7515fd4ffeea55210d66f9e11ac
robertbrada <bert.blader@gmail.com>
2020-05-24 17:46:50 +0200

    work on withdrawal proof generation

--- src/components/WithdrawPage.tsx
+++ src/components/WithdrawPage.tsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { generateProof, parseNote } from '../utils/snarks-functions';
+import { DEMO_PRIVATE_KEY, NETWORK, TORNADO_PBTC_INSTANCES_ADDRESSES } from '../config';
+import { tornadoABI } from '../contracts/tornadoABI';
+
 
 interface WithdrawPageState {
     noteWithdraw: string;
     btcAddress: string;
 }
 
-class WithdrawPage extends Component<{}, WithdrawPageState> {
+interface WithdrawPageProps {
+    web3: any;
+}
+
+class WithdrawPage extends Component<WithdrawPageProps, WithdrawPageState> {
     constructor(props: any) {
         super(props);
 
@@ -19,7 +27,7 @@ class WithdrawPage extends Component<{}, WithdrawPageState> {
         // Handle change of input fields
         switch (event.target.name) {
 
-            case 'btcReceiverAddress':
+            case 'btcRecipientAddress':
                 this.setState({ btcAddress: event.target.value });
                 break;
             case 'note':
@@ -29,6 +37,59 @@ class WithdrawPage extends Component<{}, WithdrawPageState> {
                 break;
         }
     };
+
+    /**
+     * Do an ETH withdrawal
+     */
+    withdrawHandler = async () => {
+        const refund: string = '0';
+        const recipient = this.state.btcAddress;
+        const web3 = this.props.web3;
+        const { amount, deposit } = parseNote(this.state.noteWithdraw);
+
+        const tornadoAddress =
+            TORNADO_PBTC_INSTANCES_ADDRESSES[NETWORK][amount];
+
+        const tornado = new web3.eth.Contract(
+            tornadoABI,
+            tornadoAddress
+        );
+
+        const { proof, args } = await generateProof({ deposit, recipient, refund, tornado })
+        args[2] = recipient;
+
+        // private key for demo purposes only
+        const senderPrivateKey = DEMO_PRIVATE_KEY;
+        const accountSender = web3.eth.accounts.privateKeyToAccount(senderPrivateKey);
+
+        const nonce = await web3.eth.getTransactionCount(accountSender.address);
+        const txData = await tornado.methods.withdraw(proof, ...args).encodeABI();
+
+        // sing the transaction with user's private key
+        const txSigned = await web3.eth.accounts.signTransaction(
+            {
+                nonce: nonce,
+                to: tornadoAddress,
+                value: refund.toString(),
+                data: txData,
+                gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
+                gas: web3.utils.toHex(1e6),
+            },
+            '0x' + senderPrivateKey
+        )
+
+        console.log('Submitting withdraw transaction')
+        web3.eth
+            .sendSignedTransaction(txSigned.rawTransaction)
+            .on('transactionHash', function (hash: string) {
+                console.log(['transferToStaging Trx Hash:' + hash]);
+            })
+            .on('receipt', function (receipt: any) {
+                console.log(['transferToStaging Receipt:', receipt]);
+            })
+            .on('error', console.error);
+    }
+
 
     render() {
         return <div className='withdraw-wrapper'>
@@ -41,20 +102,20 @@ class WithdrawPage extends Component<{}, WithdrawPageState> {
                     onChange={this.handleChange}
                 />
             </label>
-            <br/>
+            <br />
             <label className='btc-recipient-label'>
                 <b>Recipient's Bitcoin Address:</b>
                 <input
-                    name="btcReceiverAddress"
+                    name="btcRecipientAddress"
                     type="text"
                     value={this.state.btcAddress}
                     onChange={this.handleChange}
                 />
             </label>
-            <button className='hover-button withdraw-button'>Withdraw</button>
+            <button className='hover-button withdraw-button' onClick={this.withdrawHandler}>Withdraw</button>
         </div>;
-    };
+    }
 
-};
+}
 
 export default WithdrawPage;
